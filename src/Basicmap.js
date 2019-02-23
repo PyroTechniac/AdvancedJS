03068c6a22995b507674afcde751c15eae1bd357
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-02-23 12:12:32 -0500

    fixed basicmap set

--- src/Basicmap.js
+++ src/Basicmap.js
@@ -1,7 +1,7 @@
 const Err = require('./Error');
 const Admap = require('./Admap');
 /**
- * A Basicmap structure with the same utility as {@link Admap}, but can only have Strings and Numbers as keys
+ * A Basicmap structure with the same utility methods as {@link Admap}, but can only have Strings and Numbers as keys
  * @extends {Admap}
  */
 class Basicmap extends Admap {
@@ -10,6 +10,11 @@ class Basicmap extends Admap {
         this.keyArray().forEach(element => {
             if (element.constructor !== Number && element.constructor !== String) throw new Err(`A Basic Map can only have Strings and Numbers as it's keys, found ${element.constructor.name}`);
         });
+    }
+
+    set(key, value) {
+        if (key.constructor !== Number && key.constructor !== String) throw new Err(`A Basicmap can only hold strings or numbers, found type ${key.constructor.name}`, 'BasicmapTypeError');
+        return super.set(key, value);
     }
 }
 
