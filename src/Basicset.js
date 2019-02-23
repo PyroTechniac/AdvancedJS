f14670fb6d8a769b746833db9e453810788e79a4
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-02-23 12:14:05 -0500

    added basicset

---
+++ src/Basicset.js
@@ -0,0 +1,22 @@
+const Err = require('./Adset');
+const Adset = require('./Adset');
+/**
+ * A Basicset structure with the same utility methods as {@link Adset}, but can only store Strings and Numbers
+ */
+class Basicset extends Adset {
+    constructor(iterable) {
+        super(iterable);
+        if (this.array().map(ele => {
+            return ele.constructor.name;
+        }).includes('Object') || this.array().map(ele => {
+            return ele.constructor.name;
+        }).includes('Array')) throw new Err('A Basicset can only hold Numbers or Strings', 'BasicsetInitializationError');
+    }
+
+    add(val) {
+        if (val.constructor !== String && val.constructor !== Number) throw new Err('A basicset can only store numbers and strings', 'BasicsetTypeError');
+        return super.add(val);
+    }
+}
+
+module.exports = Basicset;
