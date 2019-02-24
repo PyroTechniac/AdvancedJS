68224c319038f1570f3d356370213c0c3d9011da
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-02-23 22:52:49 -0500

    added more methods for util

--- src/Util.js
+++ src/Util.js
@@ -1,5 +1,7 @@
 const Err = require('./Error');
 
+const { promisify } = require('util');
+const { exec } = require('child_process');
 /**
  * A Utility class with several methods
  */
@@ -37,5 +39,25 @@ class Util {
         return input && input.constructor === Object;
     }
 }
+
+/**
+ * Promisified version of {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout setTimeout} to be used with await
+ * @param {Number} delay The amount of time in ms to delay
+ * @param {*} [args] Any args to pass to the .then (mostly pointless in this form)
+ * @returns {Promise<*>} The args value passed in
+ * @method
+ * @static
+ */
+Util.sleep = promisify(setTimeout);
+
+/**
+ * Promisified version of {@link https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback} to be used with await
+ * @param {String} command The command to run
+ * @param {external:ExecOptions} [options] The options to pass to exec
+ * @returns {Promise<{ stdout: string, stderr: string}>} The promise
+ * @method
+ * @static
+ */
+Util.exec = promisify(exec);
 
 module.exports = Util;
