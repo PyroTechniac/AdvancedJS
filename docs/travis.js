21180a914350bbef217f39082f8c957a94fcd1fa
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-02-23 21:21:03 -0500

    modified travis docs and travis script itself

--- docs/travis.js
+++ docs/travis.js
@@ -1,3 +1,3 @@
 const jsdoc2md = require('jsdoc-to-markdown');
 
-jsdoc2md.render({ files: './src/*.js' }).then(data => console.log(data)).catch(err => console.error(err));
+jsdoc2md.render({ files: './src/*.js' }).then(data => console.log('Finished Builds Docs')).catch(err => console.error(err));
